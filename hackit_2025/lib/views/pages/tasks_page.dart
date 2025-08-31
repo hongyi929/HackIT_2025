2e8f17e93eb26089c3b42715de057680611e9f55
Hong Yi <limhongyi0@gmail.com>
2025-08-31 19:59:43 +0800

    Added hive dependencies and testing in tasks_page

--- hackit_2025/lib/views/pages/tasks_page.dart
+++ hackit_2025/lib/views/pages/tasks_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({super.key});
@@ -8,8 +9,80 @@ class TasksPage extends StatefulWidget {
 }
 
 class _TasksPageState extends State<TasksPage> {
+  final myBox = Hive.box("task_box");
+
+  void writeData() {
+    myBox.put(1, "Mitch");
+  }
+
+  void readData() {
+    print(myBox.get(1));
+  }
+
+  void deleteData() {
+    myBox.delete(1);
+  }
+
+  TextEditingController testController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Tasks'));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: testController,
+              onEditingComplete: () {
+                setState(() {
+                  myBox.put(testController.text, testController.text);
+                });
+              },
+            ),
+            MaterialButton(
+              onPressed: () {
+                writeData();
+              },
+              child: Text("Write data"),
+              color: Colors.blue,
+            ),
+            MaterialButton(
+              onPressed: () {
+                readData();
+              },
+              child: Text("Read data"),
+              color: Colors.blue,
+            ),
+            MaterialButton(
+              onPressed: () {
+                deleteData();
+              },
+              child: Text("Delete data"),
+              color: Colors.blue,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: myBox.length,
+                itemBuilder: (context, index) {
+                  String name = myBox.getAt(index);
+                  return ListTile(
+                    leading: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          myBox.deleteAt(index);
+                        });
+                      },
+                      icon: Icon(Icons.remove),
+                    ),
+                    title: Text("$name"),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
